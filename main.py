bc735fade3bdedd01723177af4155afc54883a4b
Davron <152484311+Davron030901@users.noreply.github.com>
2024-02-29 23:59:51 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle,Screen
+
+timy_the_turtle=Turtle()
+timy_the_turtle.shape("turtle")
+timy_the_turtle.color("green","red")
+# timy_the_turtle.forward(100)
+# timy_the_turtle.left(90)
+# timy_the_turtle.forward(100)
+# timy_the_turtle.left(90)
+# timy_the_turtle.forward(100)
+# timy_the_turtle.left(90)
+# timy_the_turtle.forward(100)
+for _ in range(4):
+    timy_the_turtle.forward(100)
+    timy_the_turtle.left(90)
+
+
+
+
+# screen=Screen()
+# screen.exitonclick()
+
